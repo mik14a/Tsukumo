8e51576b17825fa5ae46835ccf7609fa8a83acf8
mik14a <mik14a@gmail.com>
2025-03-20 05:18:33 +0900

    Consolidate model specification to request object

--- OpenAI/Interfaces/IChatCompletionService.cs
+++ OpenAI/Interfaces/IChatCompletionService.cs
@@ -10,11 +10,9 @@ namespace Tsukumo.OpenAI.Interfaces
         public string RequestUri { get; }
 
         Task<Response> CreateChatCompletion(Request request,
-                                            string modelId = null,
                                             CancellationToken cancellationToken = default);
 
         IAsyncEnumerable<Response> CreateChatCompletionAsStream(Request request,
-                                                                string modelId = null,
                                                                 CancellationToken cancellationToken = default);
     }
 }
